3613bcc2b5311232890c48e81e05637047aefba1
Mark Dostal <mhdostal@aol.com>
2019-07-31 16:48:20 -0500

    Clean up and simplifiy.

--- Examples/ArcGISToolkitExamples/Misc/Plane.swift
+++ Examples/ArcGISToolkitExamples/Misc/Plane.swift
@@ -1,47 +1,46 @@
-/*
-See LICENSE folder for this sample’s licensing information.
+//
+// Copyright 2019 Esri.
 
-Abstract:
-Convenience class for visualizing Plane extent and geometry
-*/
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import ARKit
 
+/// Helper class to visualize a plane found by ARKit
 class Plane: SCNNode {
     
-    let extentNode: SCNNode
-    var classificationNode: SCNNode?
-    
-    /// - Tag: VisualizePlane
+    let node: SCNNode
     init(anchor: ARPlaneAnchor, in sceneView: ARSCNView) {
         // Create a node to visualize the plane's bounding rectangle.
-        let extentPlane: SCNPlane = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
-        extentNode = SCNNode(geometry: extentPlane)
-        extentNode.simdPosition = anchor.center
+        let extent: SCNPlane = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
+        node = SCNNode(geometry: extent)
+        node.simdPosition = anchor.center
         
         // `SCNPlane` is vertically oriented in its local coordinate space, so
         // rotate it to match the orientation of `ARPlaneAnchor`.
-        extentNode.eulerAngles.x = -.pi / 2
+        node.eulerAngles.x = -.pi / 2
 
         super.init()
 
-        self.setupExtentVisualStyle()
+        node.opacity = 0.6
+        guard let material = node.geometry?.firstMaterial
+            else { fatalError("SCNPlane always has one material") }
+        
+        material.diffuse.contents = UIColor.blue
 
-        // Add the plane extent as child node so they appear in the scene.
-        addChildNode(extentNode)
+        // Add the plane node as child node so they appear in the scene.
+        addChildNode(node)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupExtentVisualStyle() {
-        // Make the extent visualization semitransparent to clearly show real-world placement.
-        extentNode.opacity = 0.6
-        
-        guard let material = extentNode.geometry?.firstMaterial
-            else { fatalError("SCNPlane always has one material") }
-        
-        material.diffuse.contents = UIColor.blue
     }
 }
