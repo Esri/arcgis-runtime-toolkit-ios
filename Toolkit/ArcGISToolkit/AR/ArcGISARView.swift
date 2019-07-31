143c5148848150d61cb9d7f391dfc1697de408a8
Mark Dostal <mhdostal@aol.com>
2019-07-31 12:44:17 -0500

    Fix doc typo.

--- Toolkit/ArcGISToolkit/AR/ArcGISARView.swift
+++ Toolkit/ArcGISToolkit/AR/ArcGISARView.swift
@@ -222,7 +222,7 @@ public class ArcGISARView: UIView {
     
     /// Starts device tracking.
     ///
-    /// - Parameter completion: The completion handler called when start tracking completes.  If it tracking starts successfully, the `error` property will be nil; if tracking fails to start, the error will be non-nil and contain the reason for failure.
+    /// - Parameter completion: The completion handler called when start tracking completes.  If tracking starts successfully, the `error` property will be nil; if tracking fails to start, the error will be non-nil and contain the reason for failure.
     public func startTracking(_ completion: ((_ error: Error?) -> Void)? = nil) {
         // We have a location data source that needs to be started.
         if let locationDataSource = self.locationDataSource {
