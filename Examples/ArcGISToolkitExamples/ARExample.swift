09a285caad3fa5afbd96d0739943c8ad38c8986e
Mark Dostal <mhdostal@aol.com>
2019-07-31 09:09:08 -0500

    Update examples and move them to an extension.

--- Examples/ArcGISToolkitExamples/ARExample.swift
+++ Examples/ArcGISToolkitExamples/ARExample.swift
@@ -125,8 +125,9 @@ class ARExample: UIViewController {
 
         // Set up the `sceneInfo` array with our scene init functions and labels.
         sceneInfo.append(contentsOf: [(sceneFunction: streetsScene, label: "Streets - Full Scale"),
-                                      (sceneFunction: everestScene, label: "Everest - Tabletop"),
-                                      (sceneFunction: broncosStadiumScene, label: "Broncos Stadium - Tabletop"),
+                                      (sceneFunction: pointCloudScene, label: "Point Cloud - Tabletop"),
+                                      (sceneFunction: yosemiteScene, label: "Yosemite - Tabletop"),
+                                      (sceneFunction: borderScene, label: "US - Mexico Border - Tabletop"),
                                       (sceneFunction: emptyScene, label: "Empty - Full Scale")])
         
         // Use the first sceneInfo to create and set the scene.
@@ -198,106 +199,6 @@ class ARExample: UIViewController {
         UIView.animate(withDuration: 0.25) {
             controller.view.alpha = controller.view.alpha == 1.0 ? 0.0 : 1.0
         }
-    }
-
-    // MARK: Scene Init Functions
-    
-    /// Creates a scene based on the Streets base map.
-    /// Mode:  Full-Scale AR
-    ///
-    /// - Returns: The new scene.
-    private func streetsScene() -> AGSScene {
-        
-        // Create scene with the streets basemap.
-        let scene = AGSScene(basemapType: .streets)
-        addElevationSource(toScene: scene)
-        
-        // Set the location data source so we use our GPS location as the originCamera.
-        arView.locationDataSource = AGSCLLocationDataSource()
-        arView.translationFactor = 1
-        arView.originCamera = nil
-        return scene
-    }
-    
-    /// Creates a scene based on the Mount Everest web scene.
-    /// Mode:  Tabletop AR
-    ///
-    /// - Returns: The new scene.
-    private func everestScene() -> AGSScene {
-        // Create scene using the Everest web scene.
-        let portal = AGSPortal.arcGISOnline(withLoginRequired: false)
-        let portalItem = AGSPortalItem(portal: portal, itemID: "27f76008eeb04765b8a94d998aaa46c7")
-        let scene = AGSScene(item: portalItem)
-        
-        scene.load { [weak self] (error) in
-            if let error = error {
-                self?.statusViewController?.errorMessage = error.localizedDescription
-                return
-            }
-            // Set camera to Everest summit.
-            self?.arView.originCamera = AGSCamera(latitude: 27.988153, longitude: 86.925174, altitude: 8868.069399, heading: 159.56, pitch: 0.00, roll: 0.00)
-            self?.arView.translationFactor = 1000
-        }
-        
-        // Clear the location data source, as we're setting the originCamera directly.
-        arView.locationDataSource = nil
-        return scene
-    }
-    
-    /// Creates a scene based on the Broncos stadium web scene.
-    /// Mode:  Tabletop AR
-    ///
-    /// - Returns: The new scene.
-    private func broncosStadiumScene() -> AGSScene {
-        // Create scene using WebScene of the Broncos stadium
-        let portal = AGSPortal.arcGISOnline(withLoginRequired: false)
-        let portalItem = AGSPortalItem(portal: portal, itemID: "72460f2c5b4048339433afedcb2369e1")
-        let scene = AGSScene(item: portalItem)
-        
-        scene.load { [weak self] (error) in
-            if let error = error {
-                self?.statusViewController?.errorMessage = error.localizedDescription
-                return
-            }
-            // Set the originCamera to be the initial viewpoint of the web scene.
-            self?.arView.originCamera = scene.initialViewpoint.camera
-            self?.arView.translationFactor = 1000
-        }
-        
-        // Turn off background grid.
-        scene.baseSurface?.backgroundGrid.isVisible = false
-        
-        // Clear the location data source, as we're setting the originCamera directly.
-        arView.locationDataSource = nil
-        return scene
-    }
-    
-    /// Creates an empty scene with an elevation source.
-    /// Mode:  Full-Scale AR
-    ///
-    /// - Returns: The new scene.
-    private func emptyScene() -> AGSScene {
-        let scene = AGSScene()
-        addElevationSource(toScene: scene)
-        
-        // Set the location data source so we use our GPS location as the originCamera.
-        arView.locationDataSource = AGSCLLocationDataSource()
-        arView.translationFactor = 1
-        arView.originCamera = nil
-        return scene
-    }
-
-    /// Adds an elevation source to the given `scene`.
-    ///
-    /// - Parameter scene: The scene to add the elevation source to.
-    private func addElevationSource(toScene scene: AGSScene) {
-        let elevationSource = AGSArcGISTiledElevationSource(url: URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!)
-        let surface = AGSSurface()
-        surface.elevationSources = [elevationSource]
-        surface.name = "baseSurface"
-        surface.isEnabled = true
-        surface.backgroundGrid.isVisible = false
-        scene.baseSurface = surface
     }
 }
 
@@ -396,5 +297,185 @@ extension ARExample: UIAdaptivePresentationControllerDelegate {
     func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
         // show presented controller as popovers even on small displays
         return .none
+    }
+}
+
+extension ARExample {
+    //
+    // These methods create the scenes and perform other intitialization required to set up the AR experiences.
+    //
+    
+    /// Creates a scene based on the Streets base map.
+    /// Mode:  Full-Scale AR
+    ///
+    /// - Returns: The new scene.
+    private func streetsScene() -> AGSScene {
+        
+        // Create scene with the streets basemap.
+        let scene = AGSScene(basemapType: .streets)
+        addElevationSource(toScene: scene)
+        
+        // Set the location data source so we use our GPS location as the originCamera.
+        arView.locationDataSource = AGSCLLocationDataSource()
+        arView.originCamera = nil
+        arView.translationFactor = 1
+        return scene
+    }
+    
+    /// Creates a scene based on a point cloud layer.
+    /// Mode:  Tabletop AR
+    ///
+    /// - Returns: The new scene.
+    private func pointCloudScene() -> AGSScene {
+        // Create scene using a portalItem of the point cloud layer.
+        let portal = AGSPortal.arcGISOnline(withLoginRequired: false)
+        let portalItem = AGSPortalItem(portal: portal, itemID: "fc3f4a4919394808830cd11df4631a54")
+        let layer = AGSPointCloudLayer(item: portalItem)
+        let scene = AGSScene()
+        addElevationSource(toScene: scene)
+        scene.operationalLayers.add(layer)
+        
+        layer.load { [weak self] (error) in
+            if let error = error {
+                self?.statusViewController?.errorMessage = error.localizedDescription
+                return
+            }
+
+            guard let extent = layer.fullExtent else { return }
+            let center = extent.center
+            
+            // Create the origin camera at the center point of the data.  This will ensure the data is anchored to the table.
+            let camera = AGSCamera(latitude: center.y, longitude: center.x, altitude: 0, heading: 0, pitch: 0, roll: 0)
+            self?.arView.originCamera = camera
+            self?.arView.translationFactor = 2000
+        }
+        
+        // Clear the location data source, as we're setting the originCamera directly.
+        arView.locationDataSource = nil
+        return scene
+    }
+    
+    /// Creates a scene centered on Yosemite National Park.
+    /// Mode:  Tabletop AR
+    ///
+    /// - Returns: The new scene.
+    private func yosemiteScene() -> AGSScene {
+        let scene = AGSScene()
+        addElevationSource(toScene: scene)
+        
+        // Create the Yosemite layer.
+        let layer = AGSIntegratedMeshLayer(url: URL(string:"https://tiles.arcgis.com/tiles/FQD0rKU8X5sAQfh8/arcgis/rest/services/VRICON_Yosemite_Sample_Integrated_Mesh_scene_layer/SceneServer")!)
+        scene.operationalLayers.add(layer)
+        scene.load { [weak self, weak scene] (error) in
+            if let error = error {
+                self?.statusViewController?.errorMessage = error.localizedDescription
+                return
+            }
+            
+            // Get the center point of the layer's extent.
+            guard let layer = scene?.operationalLayers.firstObject as? AGSLayer else { return }
+            guard let extent = layer.fullExtent else { return }
+            let center = extent.center
+            
+            scene?.baseSurface?.elevationSources.first?.load { (error) in
+                if let error = error {
+                    self?.statusViewController?.errorMessage = error.localizedDescription
+                    return
+                }
+                
+                // Find the elevation of the layer at the center point.
+                scene?.baseSurface?.elevation(for: center, completion: { (elevation, error) in
+                    if let error = error {
+                        self?.statusViewController?.errorMessage = error.localizedDescription
+                        return
+                    }
+                    
+                    // Create the origin camera at the center point and elevation of the data.  This will ensure the data is anchored to the table.
+                    let camera = AGSCamera(latitude: center.y, longitude: center.x, altitude: elevation, heading: 0, pitch: 0, roll: 0)
+                    self?.arView.originCamera = camera
+                    self?.arView.translationFactor = 1000
+                })
+            }
+        }
+        
+        // Clear the location data source, as we're setting the originCamera directly.
+        arView.locationDataSource = nil
+        return scene
+    }
+    
+    /// Creates a scene centered the US-Mexico border.
+    /// Mode:  Tabletop AR
+    ///
+    /// - Returns: The new scene.
+    private func borderScene() -> AGSScene {
+        let scene = AGSScene()
+        addElevationSource(toScene: scene)
+        
+        // Create the border layer.
+        let layer = AGSIntegratedMeshLayer(url: URL(string:"https://tiles.arcgis.com/tiles/FQD0rKU8X5sAQfh8/arcgis/rest/services/VRICON_SW_US_Sample_Integrated_Mesh_scene_layer/SceneServer")!)
+        scene.operationalLayers.add(layer)
+        scene.load { [weak self, weak scene] (error) in
+            if let error = error {
+                self?.statusViewController?.errorMessage = error.localizedDescription
+                return
+            }
+            
+            // Get the center point of the layer's extent.
+            guard let layer = scene?.operationalLayers.firstObject as? AGSLayer else { return }
+            guard let extent = layer.fullExtent else { return }
+            let center = extent.center
+            
+            scene?.baseSurface?.elevationSources.first?.load { (error) in
+                if let error = error {
+                    self?.statusViewController?.errorMessage = error.localizedDescription
+                    return
+                }
+                
+                // Find the elevation of the layer at the center point.
+                scene?.baseSurface?.elevation(for: center, completion: { (elevation, error) in
+                    if let error = error {
+                        self?.statusViewController?.errorMessage = error.localizedDescription
+                        return
+                    }
+                    
+                    // Create the origin camera at the center point and elevation of the data.  This will ensure the data is anchored to the table.
+                    let camera = AGSCamera(latitude: center.y, longitude: center.x, altitude: elevation, heading: 0, pitch: 0, roll: 0)
+                    self?.arView.originCamera = camera
+                    self?.arView.translationFactor = 1000
+                })
+            }
+        }
+        
+        // Clear the location data source, as we're setting the originCamera directly.
+        arView.locationDataSource = nil
+        return scene
+    }
+
+    /// Creates an empty scene with an elevation source.
+    /// Mode:  Full-Scale AR
+    ///
+    /// - Returns: The new scene.
+    private func emptyScene() -> AGSScene {
+        let scene = AGSScene()
+        addElevationSource(toScene: scene)
+        
+        // Set the location data source so we use our GPS location as the originCamera.
+        arView.locationDataSource = AGSCLLocationDataSource()
+        arView.originCamera = nil
+        arView.translationFactor = 1
+        return scene
+    }
+    
+    /// Adds an elevation source to the given `scene`.
+    ///
+    /// - Parameter scene: The scene to add the elevation source to.
+    private func addElevationSource(toScene scene: AGSScene) {
+        let elevationSource = AGSArcGISTiledElevationSource(url: URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!)
+        let surface = AGSSurface()
+        surface.elevationSources = [elevationSource]
+        surface.name = "baseSurface"
+        surface.isEnabled = true
+        surface.backgroundGrid.isVisible = false
+        scene.baseSurface = surface
     }
 }
