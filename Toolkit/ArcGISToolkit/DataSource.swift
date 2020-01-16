123f87b57f0f4ee5419c8fd00bd04cb1d561272e
Mark Dostal <mhdostal@aol.com>
2020-01-16 13:46:30 -0600

    Cleanup and additional test for boundary conditions.

--- Toolkit/ArcGISToolkit/DataSource.swift
+++ Toolkit/ArcGISToolkit/DataSource.swift
@@ -1,5 +1,5 @@
 //
-// Copyright 2019 Esri.
+// Copyright 2020 Esri.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,8 +15,10 @@
 import UIKit
 import ArcGIS
 
-/// The data source is used to represent an array of `AGSLayerContent` for use in a variety of implementations.  It is iinitialized with either an array of `AGSLayerContent`
-/// or an `AGSGeoView` of which the operational and base map layers (`AGSLayerContent`) are identified.
+/// The data source is used to represent an array of `AGSLayerContent` for use in a variety of
+/// implementations.  It is iinitialized with either an array of `AGSLayerContent`
+/// or an `AGSGeoView` from whose `AGSMap` or `AGSScene`the  operational and
+/// base map layers (`AGSLayerContent`) are extracted.
 public class DataSource: NSObject {
     /// Returns a `DataSource` initialized with the given `AGSLayerContent` array..
     /// - Parameter layers: The array of `AGSLayerContent`.
@@ -26,8 +28,10 @@ public class DataSource: NSObject {
         layerContents.append(contentsOf: layers)
     }
     
-    /// Returns a `DataSource` initialized with the operational and base map layers of a map or scene in an `AGSGeoView`.
-    /// - Parameter geoView: The `AGSGeoView` containing the map/scene's operational and base map layers.
+    /// Returns a `DataSource` initialized with the operational and base map layers of a
+    /// map or scene in an `AGSGeoView`.
+    /// - Parameter geoView: The `AGSGeoView` containing the map/scene's
+    /// operational and base map layers.
     /// - Since: 100.8.0
     public init(geoView: AGSGeoView) {
         super.init()
@@ -35,7 +39,8 @@ public class DataSource: NSObject {
         geoViewDidChange(nil)
     }
     
-    /// The `AGSGeoView` containing either an `AGSMap` or `AGSScene` with the operational and base map layers to use as data.
+    /// The `AGSGeoView` containing either an `AGSMap` or `AGSScene` with the operational and
+    /// base map layers to use as data.
     /// If the `DataSource` was initialized with an array of `AGSLayerContent`, `goeView` will be nil.
     /// - Since: 100.8.0
     public private(set) var geoView: AGSGeoView? {
@@ -44,16 +49,19 @@ public class DataSource: NSObject {
         }
     }
 
-    /// The list of all layers used to generate the TOC/Legend, read-only.  It contains both the operational layers of the map/scene and
-    /// the reference and base layers of the basemap.  The order of the layer contents is the order in which they are drawn
-    /// in a map or scene:  bottom up (the first layer in the array is at the bottom and drawn first; the last layer is at the top and drawn last).
+    /// The list of all layers used to generate the TOC/Legend, read-only.  It contains both the
+    /// operational layers of the map/scene and the reference and base layers of the basemap.
+    /// The order of the layer contents is the order in which they are drawn
+    /// in a map or scene:  bottom up (the first layer in the array is at the bottom and drawn first; the last
+    /// layer is at the top and drawn last).
     /// - Since: 100.8.0
     public private(set) var layerContents = [AGSLayerContent]()
     
     private func geoViewDidChange(_ previousGeoView: AGSGeoView?) {
         if let mapView = geoView as? AGSMapView {
             mapView.map?.load { [weak self] (error) in
-                guard let self = self, let mapView = self.geoView as? AGSMapView else { return }
+                guard let self = self,
+                    let mapView = self.geoView as? AGSMapView else { return }
                 if let error = error {
                     print("Error loading map: \(error)")
                 } else {
@@ -63,9 +71,10 @@ public class DataSource: NSObject {
             }
         } else if let sceneView = geoView as? AGSSceneView {
             sceneView.scene?.load { [weak self] (error) in
-                guard let self = self, let sceneView = self.geoView as? AGSSceneView else { return }
+                guard let self = self,
+                    let sceneView = self.geoView as? AGSSceneView else { return }
                 if let error = error {
-                    print("Error loading map: \(error)")
+                    print("Error loading scene: \(error)")
                 } else {
                     self.layerContents = sceneView.scene?.operationalLayers as? [AGSLayerContent] ?? []
                     self.appendBasemap(sceneView.scene?.basemap)
@@ -74,7 +83,7 @@ public class DataSource: NSObject {
         }
     }
     
-    func appendBasemap(_ basemap: AGSBasemap?) {
+    private func appendBasemap(_ basemap: AGSBasemap?) {
         guard let basemap = basemap else { return }
         
         basemap.load { [weak self] (error) in
--- Toolkit/ArcGISToolkitTests/DataSourceTests.swift
+++ Toolkit/ArcGISToolkitTests/DataSourceTests.swift
@@ -1,5 +1,5 @@
 //
-// Copyright 2019 Esri.
+// Copyright 2020 Esri.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -57,6 +57,57 @@ class DataSourceTests: XCTestCase {
         let dataSource = DataSource(geoView: sceneView)
         XCTAssertEqual(layerCount(scene: scene), dataSource.layerContents.count)
     }
+    
+    /// Tests boundary conditions by verifying the `layerContents` property returns the correct number of layerContents
+    func testBoundaryConditions() {
+        //
+        // Test with no basemap.
+        //
+        let sceneView = AGSSceneView()
+        let scene = AGSScene()
+        sceneView.scene = scene
+
+        let layerContents = generateLayerContents()
+        scene.operationalLayers.addObjects(from: layerContents)
+
+        // Wait for the scene to load.  This allows the observers to be set up.
+        XCTLoad(scene)
+
+        // Create the dataSource.
+        var dataSource = DataSource(geoView: sceneView)
+        XCTAssertEqual(layerCount(scene: scene), dataSource.layerContents.count)
+        
+        //
+        // Test with no operational layers.
+        //
+        var mapView = AGSMapView()
+        var map = AGSMap(basemap: .streets())
+        mapView.map = map
+        
+        // Wait for the map to load.  This allows the observers to be set up.
+        XCTLoad(map)
+        
+        // Create the dataSource.
+        dataSource = DataSource(geoView: mapView)
+        XCTAssertEqual(layerCount(map: map), dataSource.layerContents.count)
+        
+        //
+        // Test with no layers at all.
+        //
+        mapView = AGSMapView()
+        map = AGSMap(spatialReference: .wgs84())
+        mapView.map = map
+        
+        // Wait for the map to load.  This allows the observers to be set up.
+        XCTLoad(map)
+        
+        // Create the dataSource.
+        dataSource = DataSource(geoView: mapView)
+        XCTAssertEqual(layerCount(map: map), dataSource.layerContents.count)
+        XCTAssertEqual(dataSource.layerContents.count, 0)
+    }
+
+    // MARK: Internal
     
     /// Generates a list of predefined layerContents for testing.
     func generateLayerContents() -> [AGSLayerContent] {
